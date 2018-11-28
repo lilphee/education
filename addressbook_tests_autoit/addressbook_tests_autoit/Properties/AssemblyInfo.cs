dfba7a674c1bfe4a773d50210704d23be8dce2fa
Natalia Filatova <lilphee@mail.ru>
2018-11-28 17:36:43 +0300

    HW18

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1b0a80ae-0e4a-4c3d-a80e-d2624742c281")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+	[TestFixture]
+	public class GroupRemovalTests : TestBase
+	{
+		[Test]
+		public void TestGroupRemoval()
+		{
+			List<GroupData> oldGroups = app.Groups.GetGroupList();
+			GroupData toBeRemoved = oldGroups[0];
+
+			app.Groups.Remove(toBeRemoved);
+
+			List<GroupData> newGroups = app.Groups.GetGroupList();
+
+			oldGroups.RemoveAt(0);
+			oldGroups.Sort();
+			newGroups.Sort();
+
+			Assert.AreEqual(oldGroups,newGroups);
+		}
+	}
+}
+
+
+	//GroupData groupToDelete = new GroupData("grouptodelete");
+
+	//if (!app.Groups.GroupPresent())
+	//{
+	//	app.Groups.Create(groupToDelete);
+	//}
